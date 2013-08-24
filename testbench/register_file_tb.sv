425cc79ebc9e376c5d8fa458fabc7bee8a6e20f4
Mingfei Huang <huang243@purdue.edu>
2013-08-24 14:08:56 -0400

    FPGA first working

--- testbench/register_file_tb.sv
+++ testbench/register_file_tb.sv
@@ -1,8 +1,8 @@
 /*
-  Eric Villasenor
-  evillase@gmail.com
+	Eric Villasenor
+	evillase@gmail.com
 
-  register file test bench
+	register file test bench
 */
 
 // mapped needs this
@@ -13,38 +13,103 @@
 
 module register_file_tb;
 
-  parameter PERIOD = 10;
+	parameter PERIOD = 10;
 
-  logic CLK = 0, nRST = 0;
+	logic CLK = 0, nRST = 0;
 
-  // test vars
-  int v1 = 1;
-  int v2 = 4721;
-  int v3 = 25119;
+	// test vars
+	int v1 = 1;
+	int v2 = 4721;
+	int v3 = 25119;
 
-  // clock
-  always #(PERIOD/2) CLK++;
+	// clock
+	always #(PERIOD/2) CLK++;
 
-  // interface
-  register_file_if rfif ();
-  // DUT
+	// interface
+	register_file_if rfif ();
+	// DUT
 `ifndef MAPPED
-  register_file DUT(CLK, nRST, rfif);
+	register_file DUT(CLK, nRST, rfif);
 `else
-  register_file DUT(
-    .\rfif.rdat2 (rfif.rdat2),
-    .\rfif.rdat1 (rfif.rdat1),
-    .\rfif.wdat (rfif.wdat),
-    .\rfif.rsel2 (rfif.rsel2),
-    .\rfif.rsel1 (rfif.rsel1),
-    .\rfif.wsel (rfif.wsel),
-    .\rfif.WEN (rfif.WEN),
-    .\nRST (nRST),
-    .\CLK (CLK)
-  );
+	register_file DUT(
+		.\rfif.rdat2 (rfif.rdat2),
+		.\rfif.rdat1 (rfif.rdat1),
+		.\rfif.wdat (rfif.wdat),
+		.\rfif.rsel2 (rfif.rsel2),
+		.\rfif.rsel1 (rfif.rsel1),
+		.\rfif.wsel (rfif.wsel),
+		.\rfif.WEN (rfif.WEN),
+		.\nRST (nRST),
+		.\CLK (CLK)
+	);
 `endif
 
-endmodule
+always begin
+			$monitor("
+				clk=%d\n
+				wsel=%d\n
+				wdat=%d\n
+				WEN=%d\n
+				rsel1=%d\n
+				rsel2=%d\n
+				\trdat1=%d\n
+				\trdat2=%d\n\n"
+		,CLK
+		,rfif.wsel
+		,rfif.wdat
+		,rfif.WEN
+		,rfif.rsel1
+		,rfif.rsel2
+		,rfif.rdat1
+		,rfif.rdat2);
 
-program test();
-endprogram
+			$display("\n---------------reset---------------\n\n");
+			nRST = 0;
+			#(PERIOD/2)
+			nRST = 1;
+			$display("\n---------------reset---------------\n\n");
+
+
+
+	#(PERIOD*2)
+	rfif.wsel = 0;
+	rfif.WEN = 0;
+	rfif.wdat = 0;
+	rfif.rsel1 = 0;
+	rfif.rsel2 = 0;
+
+	$display("write to $0");
+	#(PERIOD*2)// write to $0
+	rfif.wsel = 0;
+	rfif.WEN = 1;
+	rfif.wdat = 99;
+	rfif.rsel1 = 1;
+	rfif.rsel2 = 0;
+
+	$display("write to $2");
+	#(PERIOD*2)// write to $2
+	rfif.wsel = 2;
+	rfif.WEN = 1;
+	rfif.wdat = 99;
+	rfif.rsel1 = 2;
+	rfif.rsel2 = 0;
+
+	$display("write to $17");
+	#(PERIOD*2)// write to $17
+	rfif.wsel = 17;
+	rfif.WEN = 1;
+	rfif.wdat = 12345;
+	rfif.rsel1 = 2;
+	rfif.rsel2 = 17;
+
+	$display("WEN is 0");
+	#(PERIOD*2)// WEN is 0
+	rfif.wsel = 31;
+	rfif.WEN = 0;
+	rfif.wdat = 54321;
+	rfif.rsel1 = 2;
+	rfif.rsel2 = 31;
+end
+
+
+endmodule
